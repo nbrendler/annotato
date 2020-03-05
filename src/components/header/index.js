8d11d8b4e2f0733e123d5be02cf6950b72dc83dc
Amie Chen <chinghc@gmail.com>
2020-03-05 15:29:56 +0800

    add tailwind

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,14 +3,14 @@ import { Link } from "preact-router/match";
 import style from "./style.css";
 
 const Header = () => (
-  <header class={style.header}>
-    <h1>Code Is Book Now</h1>
-    <nav>
-      <Link activeClassName={style.active} href="/">
-        Home
-      </Link>
-    </nav>
-  </header>
+	<header class={style.header}>
+		<h1 className="text-black">Code Is Book Now</h1>
+		<nav>
+			<Link activeClassName={style.active} href="/">
+				Home
+			</Link>
+		</nav>
+	</header>
 );
 
 export default Header;
