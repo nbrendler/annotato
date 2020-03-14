bab783bc74f73384fa97417c3fefe6d16bcff814
Nikolai Brendler <nbrendler@gmail.com>
2020-03-14 10:10:51 +0800

    Fix netlify oauth

--- src/components/gh-button.js
+++ src/components/gh-button.js
@@ -4,9 +4,11 @@ import netlify from "netlify-auth-providers";
 
 const GithubButton = () => {
   const onClick = () => {
-    const authenticator = new netlify.default({});
+    const authenticator = new netlify({
+      site_id: "11c1e4d4-5928-457e-a3c4-9118a510f247"
+    });
     authenticator.authenticate(
-      { provider: "github", scope: "user" },
+      { provider: "github", scope: "" },
       (err, data) => {
         if (err) {
           console.error(err);
