5d73848697c77c469e812579afc98128a481c9e4
Amie Chen <chinghc@gmail.com>
2020-03-18 14:41:39 +0800

    style error

--- src/components/error.js
+++ src/components/error.js
@@ -1,7 +1,7 @@
 import { h } from "preact";
 
 export const Error = ({ message }) => {
-  return <div>{message}</div>;
+  return <div class="flex flex-1 items-center justify-center"><p class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded">{message}</p></div>;
 };
 
 export default Error;
