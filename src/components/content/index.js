f3c81e4699833cd8bb92d9669f2b7323db637c13
Nikolai Brendler <nbrendler@gmail.com>
2020-03-18 14:33:17 +0800

    Only shows docs or code to save on space

--- src/components/content/index.js
+++ src/components/content/index.js
@@ -27,11 +27,17 @@ const Content = () => {
     content = data.readme?.text;
     name = "README.md";
   }
-  const memoized = useMemo(() => {
+  const [memoized, anyDocs, anyCode] = useMemo(() => {
     let sections = [];
 
     sections = format(parse(content, name), { owner, repo_name, gh_ref });
-    return sections;
+    // Check if there's any docs at all, if not we can just render code
+    const anyDocs = sections.filter(s => s.docsHtml).length > 0;
+
+    // Check if there's any code at all, if not we can just render docs, like a
+    // markdown document
+    const anyCode = sections.filter(s => s.codeHtml).length > 0;
+    return [sections, anyDocs, anyCode];
   }, [content, name, owner, repo_name, gh_ref]);
 
   if (rootLoading || contentLoading) {
@@ -43,16 +49,20 @@ const Content = () => {
   }
 
   let sections = memoized.reduce((acc, section) => {
-    acc.push(<p className="" innerHTML={section.docsHtml} />);
+    if (anyDocs) {
+      acc.push(<p className="" innerHTML={section.docsHtml} />);
+    }
 
-    // Note: hljs gives the background color
-    // docco skips this and just colors the body and
-    // adds an absolutely-positioned white rectangle
-    acc.push(
-      <pre className={`hljs`}>
-        <code innerHTML={section.codeHtml} />
-      </pre>
-    );
+    if (anyCode) {
+      // Note: hljs gives the background color
+      // docco skips this and just colors the body and
+      // adds an absolutely-positioned white rectangle
+      acc.push(
+        <pre className={`hljs`}>
+          <code innerHTML={section.codeHtml} />
+        </pre>
+      );
+    }
     return acc;
   }, []);
 
