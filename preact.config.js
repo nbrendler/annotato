fb231372b0c6f3d58dd808395bf38190676e0001
Nikolai Brendler <nbrendler@gmail.com>
2020-03-20 14:08:54 +0800

    Fix syntax highlighting in prod

--- preact.config.js
+++ preact.config.js
@@ -1,7 +1,23 @@
-const tailwind = require("preact-cli-tailwind");
-
 module.exports = (config, env, helpers) => {
-  config = tailwind(config, env, helpers);
+  const purgecss = require("@fullhuman/postcss-purgecss")({
+    content: ["./src/**/*.js"],
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+    whitelistPatterns: [/^hljs.*/]
+  });
+
+  const postCssLoaders = helpers.getLoadersByName(config, "postcss-loader");
+  postCssLoaders.forEach(({ loader }) => {
+    const plugins = loader.options.plugins;
+
+    // Add tailwind css at the top.
+    plugins.unshift(require("tailwindcss"));
+
+    // Add PurgeCSS only in production.
+    if (env.production) {
+      plugins.push(purgecss);
+    }
+  });
+
   if (config.devServer) {
     config.devServer.historyApiFallback = {
       ...config.devServer.historyApiFallback,
