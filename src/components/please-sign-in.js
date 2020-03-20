bb4f583e3546bd82502f3e320bbb6c6175941211
Nikolai Brendler <nbrendler@gmail.com>
2020-03-20 20:30:15 +0800

    Add a screenshot

--- src/components/please-sign-in.js
+++ src/components/please-sign-in.js
@@ -1,13 +1,15 @@
 import { h } from "preact";
 
 import GithubButton from "./gh-button";
+import Screenshot from "../assets/screenshot.png";
 
 export const PleaseSignIn = () => {
   return (
     <div className="flex max-w-lg mx-auto flex-col text-center -mt-16">
       <h6 className="font-bold">Welcome to Annotato</h6>
+      <img src={Screenshot} />
       <p className="mb-2">
-        A tool designed for reading code on GitHub with lots of comments.
+        A tool designed for reading GitHub code with lots of comments.
       </p>
 
       <GithubButton />
