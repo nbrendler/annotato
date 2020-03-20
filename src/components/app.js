c97a668aaba17703689bead1020b8d4fc70ebc21
Nikolai Brendler <nbrendler@gmail.com>
2020-03-20 14:23:04 +0800

    Cleanup

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,4 @@
-import { h, Component } from "preact";
+import { h } from "preact";
 import { Router } from "preact-router";
 
 import Header from "./header";
@@ -7,30 +7,14 @@ import Redirect from "./redirect";
 // Code-splitting is automated for routes
 import Viewer from "../routes/viewer";
 
-export default class App extends Component {
-  /** Gets fired when the route changes.
-   *  @param {Object} event   "change" event from [preact-router](http://git.io/preact-router)
-   *  @param {string} event.url The newly routed URL
-   */
-  handleRoute = e => {
-    this.currentUrl = e.url;
-  };
+const App = () => (
+  <div id="app" className="w-full min-h-screen relative flex flex-col">
+    <Header />
+    <Router>
+      <Viewer path="/github.com/:owner/:repo_name/:type?/:gh_ref?/:gh_path*" />
+      <Redirect path="/" to="/github.com/nbrendler/annotato" />
+    </Router>
+  </div>
+);
 
-  render() {
-    return (
-      <div id="app" className="w-full min-h-screen relative flex flex-col">
-        <Header />
-        <Router onChange={this.handleRoute}>
-          <Viewer path="/github.com/:owner/:repo_name/:type?/:gh_ref?/:gh_path*" />
-          <Redirect path="/" to="/github.com/nbrendler/annotato" />
-
-          <Viewer
-            path="/gql-error"
-            owner="nbrendler"
-            repo_name="doesnotexist"
-          />
-        </Router>
-      </div>
-    );
-  }
-}
+export default App;
